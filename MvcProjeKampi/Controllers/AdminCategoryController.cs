c0b547f70700709a4faa4affd915bd326fb73370
Selvi Sümeyra Yıldırım <selviyldrm96@gmail.com>
2021-05-07 21:52:48 +0300

    MvcProjeKampi

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Business.Conceate;
+using Business.ValidationRules;
+using DataAccess.EntityFramework;
+using Entity.Concreate;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
